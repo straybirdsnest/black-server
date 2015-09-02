151b7c250772622a6b6a38ead565a5872c40ae46
Yang Yang <reliveyy@gmail.com>
2015-09-02 16:55:24 +0800

    Thymeleaf 解析不带结束标签的HTML

--- src/main/java/com/example/config/MvcConfig.java
+++ src/main/java/com/example/config/MvcConfig.java
@@ -1,8 +1,15 @@
 package com.example.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+import org.thymeleaf.templateresolver.TemplateResolver;
 
 @Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
@@ -10,7 +17,21 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/").setViewName("index");
-		//registry.addViewController("/register-a").setViewName("register");
 	}
+
+	/*@Bean
+	public ViewResolver viewResolver() {
+		ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+		templateResolver.setTemplateMode("LEGACYHTML5");
+		templateResolver.setPrefix("templates/");
+		templateResolver.setSuffix(".html");
+		templateResolver.setCacheable(false);
+		SpringTemplateEngine engine = new SpringTemplateEngine();
+		engine.setTemplateResolver(templateResolver);
+
+		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+		viewResolver.setTemplateEngine(engine);
+		return viewResolver;
+	}*/
 
 }
