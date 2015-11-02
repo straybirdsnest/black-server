107f588300eb58ca0e9f576f6d3fe74906c4c60d
Yang Yang <reliveyy@gmail.com>
2015-11-02 16:22:08 +0800

    完善 Token 类

--- src/main/java/com/example/utils/Cryptor.java
+++ src/main/java/com/example/utils/Cryptor.java
@@ -22,21 +22,33 @@ public class Cryptor {
         }
     }
 
-    public static String encrypt(String plainText)
-            throws Exception {
-        byte[] plainTextByte = plainText.getBytes();
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-        byte[] encryptedByte = cipher.doFinal(plainTextByte);
-        Base64.Encoder encoder = Base64.getEncoder();
-        return encoder.encodeToString(encryptedByte);
+    /**
+     * @param data
+     * @return 当出现异常的时候返回空字符串
+     */
+    public static String encrypt(byte[] data) {
+        try {
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            byte[] encryptedByte = cipher.doFinal(data);
+            Base64.Encoder encoder = Base64.getEncoder();
+            return encoder.encodeToString(encryptedByte);
+        }catch (Exception e){
+            return "";
+        }
     }
 
-    public static String decrypt(String encryptedText)
-            throws Exception {
-        Base64.Decoder decoder = Base64.getDecoder();
-        byte[] encryptedTextByte = decoder.decode(encryptedText);
-        cipher.init(Cipher.DECRYPT_MODE, secretKey);
-        byte[] decryptedByte = cipher.doFinal(encryptedTextByte);
-        return new String(decryptedByte);
+    /**
+     * @param encryptedText
+     * @return 当出现异常的时候返回 null
+     */
+    public static byte[] decrypt(String encryptedText) {
+        try {
+            Base64.Decoder decoder = Base64.getDecoder();
+            byte[] encryptedTextByte = decoder.decode(encryptedText);
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            return cipher.doFinal(encryptedTextByte);
+        } catch (Exception e){
+            return null;
+        }
     }
 }
