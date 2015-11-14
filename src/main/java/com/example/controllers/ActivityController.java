12dc4b3216856cb1a62c9b82464bcb24f308b13f
otakuplus <straybirdsnest@gmail.com>
2015-11-14 13:39:24 +0800

    暂时存档

--- src/main/java/com/example/controllers/ActivityController.java
+++ src/main/java/com/example/controllers/ActivityController.java
@@ -4,7 +4,9 @@ import com.example.config.jsonviews.ActivityView;
 import com.example.daos.ActivityRepo;
 import com.example.models.Activity;
 import com.example.models.Image;
+import com.example.models.User;
 import com.example.services.ImageService;
+import com.example.services.UserService;
 import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -14,6 +16,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Iterator;
@@ -21,6 +24,10 @@ import java.util.List;
 
 @RestController
 public class ActivityController {
+
+    @Autowired
+    UserService userService;
+
     @Autowired
     ActivityRepo activityRepo;
 
@@ -29,25 +36,34 @@ public class ActivityController {
 
     @RequestMapping(value = "/api/activities", method = RequestMethod.GET)
     @JsonView(ActivityView.ActivitySummary.class)
-    public ResponseEntity<?> getRecentActivities() {
-        final PageRequest pageRequest = new PageRequest(0, 10, Sort.Direction.DESC, "startTime");
+    public ResponseEntity<?> getRecentActivities(@RequestParam("page") Integer page, @RequestParam("size") Integer size) {
+        User currentUser = userService.getCurrentUser();
+        if (currentUser == null) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        if (page == null) {
+            page = 0;
+        }
+        if (size == null) {
+            size = 20;
+        }
+        final PageRequest pageRequest = new PageRequest(page, size, Sort.Direction.DESC, "startTime");
         Page<Activity> activities = activityRepo.findAll(pageRequest);
-        if (activities.getTotalElements() > 0) {
-            List<Activity> activityList = activities.getContent();
-            Iterator<Activity> iterator = activityList.iterator();
-            Activity activity = null;
-            Image coverImage = null;
-            while (iterator.hasNext()) {
-                activity = iterator.next();
-                coverImage = activity.getCoverImage();
-                if (coverImage != null) {
-                    activity.setCoverImageAccessToken(imageService.generateAccessToken(coverImage));
-                }
-            }
-            return new ResponseEntity<>(activityList, HttpStatus.OK);
-        } else {
+        if (activities.getTotalElements() <= 0) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+        List<Activity> activityList = activities.getContent();
+        Iterator<Activity> iterator = activityList.iterator();
+        Activity activity = null;
+        Image coverImage = null;
+        while (iterator.hasNext()) {
+            activity = iterator.next();
+            coverImage = activity.getCoverImage();
+            if (coverImage != null) {
+                activity.setCoverImageAccessToken(imageService.generateAccessToken(coverImage));
+            }
+        }
+        return new ResponseEntity<>(activityList, HttpStatus.OK);
     }
 
 }
