c76cb4cc28b964e62da591e3a2e966c164eb5b89
otakuplus <straybirdsnest@gmail.com>
2015-12-05 16:33:44 +0800

    Fixed 添加主页/的访问权限，在thymeleaf当中应当使用th:src属性来链接JavaScript

--- src/main/java/com/example/config/security/SecurityConfig.java
+++ src/main/java/com/example/config/security/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -39,6 +40,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .httpBasic().disable()
                 .authorizeRequests()
+                .antMatchers("/").permitAll()
                 .antMatchers(HttpMethod.POST, API_USER).permitAll()
                 .antMatchers(HttpMethod.GET, API_TOKEN).permitAll()
                 .antMatchers(API_AVAILABILITY + "/**").permitAll()
